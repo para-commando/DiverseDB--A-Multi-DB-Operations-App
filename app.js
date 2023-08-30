325c8f3aa516f8815bc9380477226ae2b96f2fb2
anirudh-nayak-172k <anirudh.nayak.172k@gmail.com>
2023-08-30 23:43:37 +0530

    Create Operations

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
-const { dataObjects } = require('./DatabaseSampleDataObjects');
 require('dotenv').config();
 const { getMongoDataBaseConnection } = require('./connectionUtils');
 const { getMongooseModels } = require('./mongooseModels');
 const { getMongooseSchemaObjects } = require('./mongooseSchemas');
 const validator = require('validator');
-const connection = await getMongoDataBaseConnection('MyMongoDB');
-
+const mongoose = require('mongoose');
 async function mongoDbCreateOperations(connection) {
-
   try {
     // Define a schema and create a model
+    const subSchemaConstraints = {
+      street: String,
+      city: String,
+    };
 
+    const subSchema = await getMongooseSchemaObjects({
+      mongoDatabaseConnection: connection,
+      schemaConstraints: subSchemaConstraints,
+    });
     const schemaConstraints = {
-      email2: {
+      name: {
         type: String,
         required: true,
         lowercase: true,
         unique: false,
+      },
+      age: Number,
+      email: {
+        type: String,
         validate: (value) => {
           return validator.isEmail(value);
         },
       },
-      email3: {
-        type: String,
-      },
+      createdAt: Date,
+      updatedAt: Date,
+      bestFriend: connection.SchemaTypes.ObjectId,
+      hobbies: [String],
+      address: subSchema,
     };
-    const schema = await getMongooseSchemaObjects({
+    const mainSchema = await getMongooseSchemaObjects({
       mongoDatabaseConnection: connection,
       schemaConstraints: schemaConstraints,
     });
     const model = getMongooseModels({
-      modelName: 'myModel22',
-      schema: schema,
+      modelName: 'myTestModel',
+      schema: mainSchema,
       databaseConnection: connection,
     });
     // making schema changes dynamic
     await model.schema.index(
-      { email2_1: 1 },
-      { unique: schemaConstraints.email2.unique }
+      { address_1: 1 },
+      { unique: schemaConstraints.name.unique }
     );
     // Ensure the indexes are applied
     await model.ensureIndexes();
@@ -45,9 +56,21 @@ async function mongoDbCreateOperations(connection) {
     // Insert many documents using .create() method
 
     const data = [
-      { email2: 'aba23423cd@gmail.com' },
-      { email2: 'abac34234d@gmail.com' },
-      { email2: 'abac23234d@gmail.com' },
+      {
+        name: 'john doe',
+        age: 28,
+        email: 'john@example.com',
+        createdAt: new Date('2023-08-29T00:00:00Z'),
+        updatedAt: new Date('2023-08-29T12:34:56Z'),
+        bestFriend: new mongoose.Types.ObjectId('5f41bfc57a22a9a23d3e5d0c'), // ObjectId of another document
+        hobbies: ['Reading', 'Hiking', 'Cooking'],
+        address: {
+          street: '123 Main St',
+          city: 'Anytown',
+          state: 'CA',
+          postalCode: '12345',
+        },
+      },
     ];
     const insertManyResult = await model.create(data);
     console.log(
@@ -66,12 +89,49 @@ async function mongoDbCreateOperations(connection) {
       '🚀 Total documents inserted using instance method:  ',
       insertingMultipleDocuments.length
     );
+
+    // ************************ UseCase-2 *******************************
+
+    const personSchemaConstraints = {
+      name: String,
+      age: Number,
+      bestFriend: { type: connection.SchemaTypes.ObjectId, ref: 'Person' }, // Reference to another Person document
+    };
+    const personSchema = await getMongooseSchemaObjects({
+      mongoDatabaseConnection: connection,
+      schemaConstraints: personSchemaConstraints,
+    });
+    // Create the Person model
+    const PersonModel = getMongooseModels({
+      modelName: 'Person',
+      schema: personSchema,
+      databaseConnection: connection,
+    });
+
+    const alice = new PersonModel({ name: 'Alice', age: 28 });
+    const bob = new PersonModel({ name: 'Bob', age: 30 });
+
+    // Assume alice is best friends with bob
+    alice.bestFriend = bob._id; // _id is the ObjectId of the bob document
+    await alice.save();
+    await bob.save();
   } catch (err) {
     console.error(`Error inserting documents: ${err}`);
+    throw err;
   }
-  await connection.disconnect();
 }
 
-mongoDbCreateOperations(connection).catch((error) => {
-  console.log('Error in mongoDbCreateOperations:', error);
-});
+async function mongoDatabaseCRUD_Ops() {
+  const connection = await getMongoDataBaseConnection('MyMongoDB');
+
+  try {
+    await mongoDbCreateOperations(connection);
+  } catch (err) {
+    console.log('🚀 ~ file: app.js:81 ~ err:', err);
+    throw err;
+  } finally {
+    await connection.disconnect();
+  }
+}
+
+mongoDatabaseCRUD_Ops();
