638e78a561dc394d1eb36c8c977d0a56b9348b0e
anirudh-nayak-172k <anirudh.nayak.172k@gmail.com>
2023-08-31 23:17:20 +0530

    modularized data

--- MongoDbOperations.js
+++ MongoDbOperations.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
-const { getMongooseModels } = require('./makeMongooseModels');
-const { getMongooseSchemaObjects } = require('./mongooseSchemas');
-const mongoose = require('mongoose');
 const { mongooseModels } = require('./mongooseModels');
+const { dataObjects } = require('./DatabaseSampleDataObjects');
 module.exports.mongoDbCreateOperations = async (connection) => {
   try {
     // Define a schema and create a model
@@ -11,24 +9,9 @@ module.exports.mongoDbCreateOperations = async (connection) => {
 
     // Insert many documents using .create() method
 
-    const data = [
-      {
-        name: 'john doe',
-        age: 28,
-        email: 'john@example.com',
-        createdAt: new Date('2023-08-29T00:00:00Z'),
-        updatedAt: new Date('2023-08-29T12:34:56Z'),
-        bestFriend: new mongoose.Types.ObjectId('5f41bfc57a22a9a23d3e5d0c'), // ObjectId of another document
-        hobbies: ['Reading', 'Hiking', 'Cooking'],
-        address: {
-          street: '123 Main St',
-          city: 'Anytown',
-          state: 'CA',
-          postalCode: '12345',
-        },
-      },
-    ];
-    const insertManyResult = await model.create(data);
+    const insertManyResult = await model.create(
+      dataObjects.mongDatabaseDataObjects.sampleInsertData
+    );
     console.log(
       '🚀 Total documents inserted using .create method:  ',
       insertManyResult.length
@@ -36,7 +19,9 @@ module.exports.mongoDbCreateOperations = async (connection) => {
 
     // Insert many documents using the instance method
 
-    const instances = data.map((item) => new model(item));
+    const instances = dataObjects.mongDatabaseDataObjects.sampleInsertData.map(
+      (item) => new model(item)
+    );
 
     const promises = instances.map((instance) => instance.save());
 
