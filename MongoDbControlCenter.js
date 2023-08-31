046b69453ef075da149584b88d0b8926b50e6206
anirudh-nayak-172k <anirudh.nayak.172k@gmail.com>
2023-08-31 23:13:16 +0530

    making models reusable

--- MongoDbControlCenter.js
+++ MongoDbControlCenter.js
@@ -1,10 +1,11 @@
 const { getMongoDataBaseConnection } = require('./connectionUtils');
-const {mongoDbCreateOperations} = require('./MongoDbOperations')
+const {mongoDbCreateOperations, mongoDbReadOperations} = require('./MongoDbOperations')
 module.exports.mongoDatabaseCRUD_Ops =  async () => {
     const connection = await getMongoDataBaseConnection('MyMongoDB');
   
     try {
       await mongoDbCreateOperations(connection);
+      await mongoDbReadOperations(connection);
     } catch (err) {
       console.log('🚀 ~ file: app.js:81 ~ err:', err);
       throw err;
--- MongoDbOperations.js
+++ MongoDbOperations.js
@@ -1,76 +1,13 @@
 require('dotenv').config();
-const { getMongooseModels } = require('./mongooseModels');
+const { getMongooseModels } = require('./makeMongooseModels');
 const { getMongooseSchemaObjects } = require('./mongooseSchemas');
-const validator = require('validator');
 const mongoose = require('mongoose');
-module.exports.mongoDbCreateOperations =  async (connection) => {
+const { mongooseModels } = require('./mongooseModels');
+module.exports.mongoDbCreateOperations = async (connection) => {
   try {
     // Define a schema and create a model
-    const subSchemaConstraints = {
-      street: String,
-      city: String,
-    };
-
-    const subSchema = await getMongooseSchemaObjects({
-      mongoDatabaseConnection: connection,
-      schemaConstraints: subSchemaConstraints,
-    });
-    // the uppercase:true converts string to uppercase
-    //  default: () => new Date() When you use an arrow function, it will be executed each time a new document is inserted. This ensures that the default value for the createdAt field will be the date and time when the document is inserted into the database.
-    // beware: note that these validations only work with create/save methods for inserting documents. It is preferred to use findById and .save on it or findOne and .save on it to prevent bypassing of below schema validation
-    const schemaConstraints = {
-      name: {
-        type: String,
-        required: true,
-        uppercase: true,
-        unique: false,
-        minlength: 3,
-        maxLength: 40
-      },
-      age: {
-        type: Number,
-        min:1,
-        validate:{
-          validator: age=> age <= 150,
-          message: props => `${props.path} input(${props.value}) is  greater than 150`
-        }
-      },
-      email: {
-        type: String,
-        validate: (value) => {
-          return validator.isEmail(value);
-        },
-      },
-      createdAt: {
-        type: Date,
-        immutable: true,
-        default: () => new Date(),
-      },
-      updatedAt: {
-        type: Date,
-        immutable: true,
-        default: () => new Date(),
-      },
-      bestFriend: connection.SchemaTypes.ObjectId,
-      hobbies: [String],
-      address: subSchema,
-    };
-    const mainSchema = await getMongooseSchemaObjects({
-      mongoDatabaseConnection: connection,
-      schemaConstraints: schemaConstraints,
-    });
-    const model = getMongooseModels({
-      modelName: 'myTestModel',
-      schema: mainSchema,
-      databaseConnection: connection,
-    });
-    // making schema changes dynamic
-    await model.schema.index(
-      { address_1: 1 },
-      { unique: schemaConstraints.name.unique }
-    );
+    const model = await mongooseModels.myTestModel({ connection: connection });
     // Ensure the indexes are applied
-    await model.ensureIndexes();
 
     // Insert many documents using .create() method
 
@@ -111,20 +48,9 @@ module.exports.mongoDbCreateOperations =  async (connection) => {
 
     // ************************ UseCase-2 *******************************
 
-    const personSchemaConstraints = {
-      name: String,
-      age: Number,
-      bestFriend: { type: connection.SchemaTypes.ObjectId, ref: 'Person' }, // Reference to another Person document
-    };
-    const personSchema = await getMongooseSchemaObjects({
-      mongoDatabaseConnection: connection,
-      schemaConstraints: personSchemaConstraints,
-    });
     // Create the Person model
-    const PersonModel = getMongooseModels({
-      modelName: 'Person',
-      schema: personSchema,
-      databaseConnection: connection,
+    const PersonModel = await mongooseModels.PersonModel({
+      connection: connection,
     });
 
     const alice = new PersonModel({ name: 'Alice', age: 28 });
@@ -138,4 +64,12 @@ module.exports.mongoDbCreateOperations =  async (connection) => {
     console.error(`Error inserting documents: ${err}`);
     throw err;
   }
-}
+};
+
+module.exports.mongoDbReadOperations = async (connection) => {
+  try {
+  } catch (err) {
+    console.error(`Error Finding documents: ${err}`);
+    throw err;
+  }
+};
