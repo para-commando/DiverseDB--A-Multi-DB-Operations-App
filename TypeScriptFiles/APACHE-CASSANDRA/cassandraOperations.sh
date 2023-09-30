f7fac0972a8081b9a8ec8b0606848ca4fca47901
anirudh-nayak-172k <anirudh.nayak.172k@gmail.com>
2023-10-01 00:55:53 +0530

    automation scripts integration

--- TypeScriptFiles/APACHE-CASSANDRA/cassandraOperations.sh
+++ TypeScriptFiles/APACHE-CASSANDRA/cassandraOperations.sh
@@ -1,4 +1,23 @@
-# source ./queryExecuter.sh cassandra-node-1 "CREATE KEYSPACE IF NOT EXISTS learn_cassandra_keyspaces WITH replication = {'class': 'NetworkTopologyStrategy', 'datacenter1': 2};"
+
+
+IS_IMAGE_PRESENT = source ./isImagePresent.sh 
+IS_CONSTAINER_PRESENT = source ./isContainerPresent.sh
+IS_CONTAINER_RUNNING = source ./isContainerRunning.sh
+if ["$IS_IMAGE_PRESENT"="false"]; then
+   source ./configurations/build-custom-cassandra-image.sh
+fi
+
+if ["$IS_CONSTAINER_PRESENT" = "false"]; then
+ source ./configurations/initial-cluster-setup.sh
+else if ["$IS_CONTAINER_RUNNING" = "false"]; then
+   source ./configurations/start-cluster.sh
+fi
+
 
 ./queryExecuter.sh cassandra-node-1 createOpsQueries.cql
 
+./queryExecuter.sh cassandra-node-1 readOpsQueries.cql
+
+./queryExecuter.sh cassandra-node-1 updateOpsQueries.cql
+
+./queryExecuter.sh cassandra-node-1 deleteOpsQueries.cql
--- TypeScriptFiles/APACHE-CASSANDRA/isContainerPresent.sh
+++ TypeScriptFiles/APACHE-CASSANDRA/isContainerPresent.sh
@@ -1,7 +1,7 @@
 # ooo=$(./isImagePresent.sh)
 # echo "$ooo"
 
-IMAGE_NAME="cassandra-node-21"
+IMAGE_NAME="cassandra-node-1"
 
 # Capture the result of the docker images command and grep
 RESULTs=$(docker ps -a | grep -q "$IMAGE_NAME" && echo "true" || echo "false")
