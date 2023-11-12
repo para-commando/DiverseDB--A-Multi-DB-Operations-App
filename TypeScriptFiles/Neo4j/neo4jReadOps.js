468d0880fc8d0b40aaa29ddeb88b6267eecc7193
anirudh-nayak-172k <anirudh.nayak.172k@gmail.com>
2023-11-12 15:29:25 +0530

    read ops work

--- TypeScriptFiles/Neo4j/neo4jReadOps.js
+++ TypeScriptFiles/Neo4j/neo4jReadOps.js
@@ -6,8 +6,34 @@ const readOpsSession = driver.session();
 
 module.exports.readOps = async () => {
   const cypherQuery = [
-    { query: `MATCH (N) RETURN N`, message: 'getAll success' },
-    { query: `MATCH (N:Customer) RETURN N`, message: 'getCustomer success' },
+    {
+      query: `MATCH (bookings:Bookings)-[booked_on:BOOKED_ON]->(s:Shipments)
+    WITH bookings, booked_on, s,
+         split(booked_on.BookingID_Date, '/') AS dateParts
+    WITH bookings, booked_on, s, 
+         toInteger(dateParts[2]) AS year,
+         toInteger(dateParts[0]) AS month,
+         toInteger(dateParts[1]) AS day
+    WHERE datetime({year: year, month: month, day: day}) >= datetime({year: 2023, month: 7, day: 1})
+      AND datetime({year: year, month: month, day: day}) < datetime({year: 2023, month: 8, day: 1})
+    RETURN bookings, booked_on, s`,
+      message: 'get all bookings in date range success',
+    },
+    { query: `MATCH (N) RETURN N`, message: 'get all nodes success' },
+    {
+      query: `MATCH (shipments:Shipments) 
+    where shipments.shipment_type='Next Day'
+    return shipments
+    `,
+      message: 'get shipments based on shipment types success',
+    },
+    {
+      query: `MATCH (origin:Origin)
+      WHERE origin.originLocation =~ '.*Wieczfnia.*'
+      RETURN origin;
+    `,
+      message: 'get shipments originated at a given location success',
+    },
   ];
 
   const result = runMultipleQueries({
